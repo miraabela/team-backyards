935bdb1eaf2a8da255739e65fd62581eadfd8128
mirabela <EvanYoung@FloWaters-MacBook-Air.local>
2019-10-24 19:07:54 -0600

    m

---
+++ divetable/scripts.js
@@ -0,0 +1,10 @@
+function calcPressureGroup(depth, time) {
+	if (depth < 35) {
+		alert("Depth must be greater than 35")
+	}
+	if (depth > 140) {
+		alert("Depth must be less than 140")
+	}
+
+	return
+}
